60b07718943d48aa919f4ad2919d5b6f9f20b951
Yves <cauayves5@gmail.com>
2024-02-11 12:09:14 -0300

    chore: error messge from signin route validation changed for best interpretation by user

--- src/schemas/auth-schema.ts
+++ src/schemas/auth-schema.ts
@@ -2,14 +2,16 @@ import Zod from "zod";
 
 const signupSchema = Zod.object({
   name: Zod.string(),
-  cpf: Zod.string().regex(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, "insira um cpf válido."),
+  cpf: Zod.string().regex(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, "insira um cpf válido. "),
   email: Zod.string()
     .email()
-    .regex(/@gmail\.com$/, "O email deve ser do domínio @gmail.com"),
-  password: Zod.string().min(6, "mínimo de 6 caracteres.").max(24, "máximo de 24 caracteres."),
+    .regex(/@gmail\.com$/, "O email deve ser do domínio @gmail.com "),
+  password: Zod.string().min(6, "mínimo de 6 caracteres.").max(24, "máximo de 24 caracteres. "),
 });
 const signinSchema = Zod.object({
-  email: Zod.string().email(),
+  email: Zod.string()
+    .email("deve ser um email válido ")
+    .regex(/@gmail\.com$/, "O email deve ser do domínio @gmail.com "),
   password: Zod.string(),
 });
 
